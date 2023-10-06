7a1bb77a2e88a23edea1bd20d862055c392dc4c7
DikaAntoniPutra <dikahanggara2@gmail.com>
2023-10-06 10:55:33 +0700

    update seeder dan relasi

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,6 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('username');
-            // $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
             $table->string('tempat');
             $table->string('tanggal');
             $table->string('password');
